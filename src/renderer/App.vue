ac7432a6869c7c1f5a5b2a4a3d732d0db26de467
Daniel Oswalt <oswalt@lclark.edu>
2018-05-31 13:59:42 -0700

    initial

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gatetracker2'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
