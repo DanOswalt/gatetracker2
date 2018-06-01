66b3a6cb535d04fc5f0f467e989d335c5c8ff4ab
Daniel Oswalt <oswalt@lclark.edu>
2018-05-31 19:56:23 -0700

    add sass-loader

---
+++ src/renderer/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>   
+
+<style lang="scss" scoped>
+
+</style>
